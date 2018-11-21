9b405639272eb6989604ab1ec732e3c0191b7e25
andreas <andreasberger86@hotmail.de>
2018-11-21 18:34:09 +0100

    debug

--- CBM_Benchmark/Main.cpp
+++ CBM_Benchmark/Main.cpp
@@ -45,6 +45,14 @@ main(int argc, char *argv[])
         return server();
     }
 	case 4: // runs server
+    {
+        int num_ep = atoi(argv[2]);
+        if (num_ep > 0) {
+            config->num_ep = num_ep;
+        }
+        printf("Endpoints: %d\n", config->num_ep);
+        return server();
+    }
 	case 5: // runs client
     {
         int threads = atoi(argv[1]);
@@ -64,10 +72,47 @@ main(int argc, char *argv[])
             config->total_data_size = total_data_size * 1024 * 1024;
         }
         printf("total_data_size: %d\n", config->total_data_size);
-        if (argc == 4) {
-            return server();
+        msg_size
+
+        char *addr = argv[argc-1];
+        addr = host2ip::resolve(addr);
+        if (addr == NULL) {
+            return -1;
         } else {
-            char *addr = argv[4];
+            config->addr = addr;
+        }
+        printf("addr: %s\n", config->addr);
+
+        return client(addr);
+
+    }
+        case 6: // runs client
+        {
+            int threads = atoi(argv[1]);
+            if (threads > 0) {
+                config->threads = threads;
+            }
+            printf("threads: %d\n", threads);
+
+            int num_ep = atoi(argv[2]);
+            if (num_ep > 0) {
+                config->num_ep = num_ep;
+            }
+            printf("Endpoints: %d\n", config->num_ep);
+
+            int total_data_size = atoi(argv[3]);
+            if (total_data_size > 0) {
+                config->total_data_size = total_data_size * 1024 * 1024;
+            }
+            printf("total_data_size: %d\n", config->total_data_size);
+
+            int msg_size = atoi(argv[4]);
+            if (msg_size > 0) {
+                config->msg_size = msg_size * 1024 * 1024;
+            }
+            printf("msg_size: %d\n", config->msg_size);
+
+            char *addr = argv[argc-1];
             addr = host2ip::resolve(addr);
             if (addr == NULL) {
                 return -1;
@@ -77,12 +122,12 @@ main(int argc, char *argv[])
             printf("addr: %s\n", config->addr);
 
             return client(addr);
+
         }
-    }
 	default:
     {
         fprintf(stderr, "wrong number of arguments given: %d\n", argc);
-        fprintf(stderr, "usage:\n[server auto conf] %s\n[server manual conf] %s threads num_ep count\n[client] %s threads num_ep total_data_Size(MB) serveraddr\n", argv[0]);
+        fprintf(stderr, "usage:\n[server auto conf] %s\n[server manual conf] %s threads num_ep count\n[client] %s threads num_ep total_data_Size(MB) msg_size(MB) serveraddr\n", argv[0]);
         return -1;
     }
 	}
