4b11ac8be592c0aa9a16de33597f2a32650dfe02
andreas <andreasberger86@hotmail.de>
2018-11-21 18:01:36 +0100

    debug

--- CBM_Benchmark/Endpoint.cpp
+++ CBM_Benchmark/Endpoint.cpp
@@ -176,7 +176,7 @@ int Endpoint::client_thread(struct ctx * ctxx )
                     //printf("thread[%d] iter %d: fi_write: %d\n", ctx->id, j, temp);
                     memcpy(msg_buff + msg_size * ctx->id, &temp, msg_size);*/
                     memcpy(msg_buff + offset, &message[0], msg_size);
-                    printf("msg_buff %d\n", msg_buff[arraysize-1] + 1)* sizeof(int));
+                    printf("msg_buff %d\n", (msg_buff[arraysize-1] + 1)* sizeof(int));
                     ret = fi_write(ep, msg_buff + offset, msg_size, fi_mr_desc(mr),
                                    0, keys.addr + offset, keys.rkey, ctx);
                     if (ret) {
