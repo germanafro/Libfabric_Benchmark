7f05ac15cfc66b93f523f747e74cb4b2441f1c9e
Andreas B <andreasberger86@hotmail.de>
2018-11-22 16:54:57 +0100

    maybe use array offset instead of pointer offset

--- CBM_Benchmark/Endpoint.cpp
+++ CBM_Benchmark/Endpoint.cpp
@@ -180,7 +180,7 @@ int Endpoint::client_thread(struct ctx * ctxx )
                     //printf("thread[%d] iter %d: fi_write: %d\n", ctx->id, j, temp);
                     memcpy(msg_buff + msg_size * ctx->id, &temp, msg_size);*/
 
-                    memcpy(msg_buff + offset, &message[0], msg_size);
+                    memcpy(&msg_buff[arraysize*thread], &message[0], msg_size);
                     ret = fi_write(ep, msg_buff + offset, msg_size, fi_mr_desc(mr),
                                    0, keys.addr + offset, keys.rkey, ctx);
                     if (ret) {
